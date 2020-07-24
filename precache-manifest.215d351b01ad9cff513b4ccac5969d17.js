0e9df201320d1cf2c5b6e87acd584784e3f9e77c
SwapnilMedhe <swapnil.medhe1@gmail.com>
2020-07-25 00:04:27 +0530

    Updates

---
+++ precache-manifest.215d351b01ad9cff513b4ccac5969d17.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d409b041f869a24afd7619d112718bb",
+    "url": "/quiz_app/index.html"
+  },
+  {
+    "revision": "c28036a838d3315e5327",
+    "url": "/quiz_app/static/css/2.38f92254.chunk.css"
+  },
+  {
+    "revision": "98f8cb9b0bc5219bef1a",
+    "url": "/quiz_app/static/css/main.449b6ab0.chunk.css"
+  },
+  {
+    "revision": "c28036a838d3315e5327",
+    "url": "/quiz_app/static/js/2.9d714f24.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/quiz_app/static/js/2.9d714f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f8cb9b0bc5219bef1a",
+    "url": "/quiz_app/static/js/main.2981a773.chunk.js"
+  },
+  {
+    "revision": "10180498db8ec6dc4e17",
+    "url": "/quiz_app/static/js/runtime-main.365bdf52.js"
+  },
+  {
+    "revision": "02882ce29c39d46d8cb8c0eac28ce519",
+    "url": "/quiz_app/static/media/answer.02882ce2.mp3"
+  },
+  {
+    "revision": "18b46b3e9fe20e5d255a708c7b31bec5",
+    "url": "/quiz_app/static/media/exam.18b46b3e.jpg"
+  },
+  {
+    "revision": "3a0b3b123a2a3ffd394505a9966231b8",
+    "url": "/quiz_app/static/media/materialdesignicons-webfont.3a0b3b12.woff2"
+  },
+  {
+    "revision": "3e81ae3d5da947449b00c686bfe100af",
+    "url": "/quiz_app/static/media/materialdesignicons-webfont.3e81ae3d.ttf"
+  },
+  {
+    "revision": "92b716858bfb2c48f54b8e5d996830f7",
+    "url": "/quiz_app/static/media/materialdesignicons-webfont.92b71685.woff"
+  },
+  {
+    "revision": "cdac4fb864d0816f2cbef6c78d4f6490",
+    "url": "/quiz_app/static/media/materialdesignicons-webfont.cdac4fb8.eot"
+  }
+]);
